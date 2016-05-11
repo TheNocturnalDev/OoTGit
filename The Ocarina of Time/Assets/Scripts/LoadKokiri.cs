9097a3786f5ed14378ebab221fb242894bf05f3c
Dima M <dima.m99@gmail.com>
2016-05-10 20:25:03 -0700

    Musical Overhaul
    Main Menu:
    Plays tribute to first Zelda game, then loops fairy fountain
    
    Hyrule Field:
    Has proper loop
    
    Kakariko:
    Has Proper Loop (And used non synthonic movment ver.)
    
    Kokiri Forest:
    Is a thing now
    
    Switched to OGG format (Loops flawlessy  love it)
    
    Noticed that game magically started going at 60fps when not on RDP, will
    have to figure out how to lock framerate

---
+++ The Ocarina of Time/Assets/Scripts/LoadKokiri.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadKokiri : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("toforest"))
+        {
+            SceneManager.LoadScene("Kokiri Forest");
+        }
+    }
+}
---
+++ The Ocarina of Time/Assets/Scripts/TitleThemeAudioSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleThemeAudioSwitcher : MonoBehaviour {
+    public AudioClip otherClip;
+    AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!audio.isPlaying) {
+            audio.clip = otherClip;
+            audio.Play();
+        }
+	}
+}
