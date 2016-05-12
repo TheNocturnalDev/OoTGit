8634a22377ddf02ce4b0797ae5047374e80159a1
Dima M <dima.m99@gmail.com>
2016-05-11 20:37:52 -0700

    Hyrule Field only plays morning theme when time var is between 0-16

--- The Ocarina of Time/Assets/Scripts/playmusicontime.cs
+++ The Ocarina of Time/Assets/Scripts/playmusicontime.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 using System.Collections;
 
-public class playmusicontime : MonoBehaviour {
+public class playmusicontime : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         var vars = GameObject.FindGameObjectWithTag("GameController");
         var check = vars.GetComponent<VarTracker>();
         var audio1 = GameObject.FindGameObjectWithTag("Audio1");
         var audio2 = GameObject.FindGameObjectWithTag("Audio2");
         audio2.SetActive(false);
         audio1.SetActive(false);
-        if (check.time > 0 && < 16 ) {
-          audio1.SetActive(true); // Audio 1 has both the intro and the loop, with simple scripting to use both, while Audio 2 has only the loop, whicj just plays in the ELSE Statement
+        if (check.time > 0 && check.time < 16)
+        {
+            audio1.SetActive(true); // Audio 1 has both the intro and the loop, with simple scripting to use both, while Audio 2 has only the loop, whicj just plays in the ELSE Statement
         }
-        else {
-          audio2.SetActive(true);
+        else
+        {
+            audio2.SetActive(true);
         }
+    }
 }
